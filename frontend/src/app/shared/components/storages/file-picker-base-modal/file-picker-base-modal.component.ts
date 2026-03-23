smithdouglas404/agenticopenproject

Fix storages file picker CD in zoneless mode
Adds `markForCheck()` in `FilePickerBaseModalComponent` subscribe
callbacks so breadcrumbs and loading state render after async file
list updates. Moves `zone.js` to devDependencies in package-lock.json.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

frontend/src/app/shared/components/storages/file-picker-base-modal/file-picker-base-modal.component.ts
@@ -120,9 +120,11 @@ export abstract class FilePickerBaseModalComponent extends OpModalComponent impl
           this.breadcrumbs = this.makeBreadcrumbs(storageFiles.ancestors, storageFiles.parent);
           this.storageFiles$.next(storageFiles.files);
           this.loading$.next('success');
+          this.cdRef.markForCheck();
         },
         error: (error) => {
           this.loading$.next('error');
+          this.cdRef.markForCheck();
           throw error;
         },
       });
@@ -160,9 +162,11 @@ export abstract class FilePickerBaseModalComponent extends OpModalComponent impl
           this.breadcrumbs = this.makeBreadcrumbs(storageFiles.ancestors, storageFiles.parent);
           this.storageFiles$.next(storageFiles.files);
           this.loading$.next('success');
+          this.cdRef.markForCheck();
         },
         error: (error) => {
           this.loading$.next('error');
+          this.cdRef.markForCheck();
           throw error;
         },
       });

ygerlach/openproject

frontend/src/app/shared/components/storages/file-picker-base-modal/file-picker-base-modal.component.ts
@@ -120,9 +120,11 @@ export abstract class FilePickerBaseModalComponent extends OpModalComponent impl
           this.breadcrumbs = this.makeBreadcrumbs(storageFiles.ancestors, storageFiles.parent);
           this.storageFiles$.next(storageFiles.files);
           this.loading$.next('success');
+          this.cdRef.detectChanges();
         },
         error: (error) => {
           this.loading$.next('error');
+          this.cdRef.detectChanges();
           throw error;
         },
       });
@@ -151,6 +153,7 @@ export abstract class FilePickerBaseModalComponent extends OpModalComponent impl
   protected changeLevel(ancestor:IStorageFile):void {
     this.cancelCurrentLoading();
     this.loading$.next('loading');
+    this.cdRef.detectChanges();
 
     this.loadingSubscription = this.storageFilesResourceService
       .files(makeFilesCollectionLink(this.storage._links.self, ancestor.location))
@@ -160,9 +163,11 @@ export abstract class FilePickerBaseModalComponent extends OpModalComponent impl
           this.breadcrumbs = this.makeBreadcrumbs(storageFiles.ancestors, storageFiles.parent);
           this.storageFiles$.next(storageFiles.files);
           this.loading$.next('success');
+          this.cdRef.detectChanges();
         },
         error: (error) => {
           this.loading$.next('error');
+          this.cdRef.detectChanges();
           throw error;
         },
       });